3c393642bfd61c97eda138515cb5b12d9744aea2
itsseanez <74938477+itsseanez@users.noreply.github.com>
2025-11-18 23:03:39 -0500

    Create playRound function to decides who wins and loses round

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let getComputerChoice = () => {
-    let choice = Math.floor(Math.random() * 3);
+    let choice = Math.floor(Math.random() * 3); //returns random integer from 0-2
     let computerChoice;
     switch(choice) {
         case 0:
@@ -16,8 +16,46 @@ let getComputerChoice = () => {
 };
 
 let getHumanChoice = () => {
-    let choice = prompt("rock, paper, or scissors?");
+    let choice = prompt("Rock, Paper, or Scissors?");
     return choice;
 };
 
-console.log(getHumanChoice());
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+    humanChoice= humanChoice.toLowerCase();
+    if (humanChoice === computerChoice) {
+        console.log("It's a tie.")
+    } else if (humanChoice === 'rock') {
+        if (computerChoice === 'paper') {
+            console.log("You lose! Paper beats Rock.")
+            computerScore++
+        } else {
+            console.log("You win! Rock beats Scissors.")
+            humanScore++
+        }
+    } else if (humanChoice === 'paper') {
+        if (computerChoice === 'scissors') {
+            console.log("You lose! Scissors beats Paper.")
+            computerScore++
+        } else {
+            console.log("You win! Paper beats Rock.")
+            humanScore++
+        }
+    } else if (humanChoice === 'scissors') {
+        if (computerChoice === 'rock') {
+            console.log("You lose! Rock beats Scissors.")
+            computerScore++
+        } else {
+            console.log("You win! Scissors beats Paper.")
+            humanScore++
+        }
+    }
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
+
