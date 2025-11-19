8a75816a711922ffb3ef8eb378c4d4ffa5e5e423
itsseanez <74938477+itsseanez@users.noreply.github.com>
2025-11-18 22:15:24 -0500

    Create getComputerChoice method to get a random choice by the computer

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log('hello world');
+let getComputerChoice = () => {
+    let choice = Math.floor(Math.random() * 3);
+    let computerChoice;
+    switch(choice) {
+        case 0:
+            computerChoice = 'rock';
+            break;
+        case 1:
+            computerChoice = 'paper';
+            break;
+        case 2:
+            computerChoice = 'scissors';
+            break;
+    }
+    return computerChoice;
+};
+console.log(getComputerChoice());
