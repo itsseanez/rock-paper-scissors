20eabaada261627f6e10edcee0cc575e1a7ffec7
itsseanez <74938477+itsseanez@users.noreply.github.com>
2025-11-19 14:00:37 -0500

    Create getHumanChoice logic with html button elements

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
+let humanScore = 0;
+let computerScore = 0;
+
 let getComputerChoice = () => {
     let choice = Math.floor(Math.random() * 3); //returns random integer from 0-2
     let computerChoice;
-    switch(choice) {
+    switch (choice) {
         case 0:
             computerChoice = 'rock';
             break;
@@ -15,12 +18,49 @@ let getComputerChoice = () => {
     return computerChoice;
 };
 
-let getHumanChoice = () => {
-    let choice = prompt("Rock, Paper, or Scissors?");
-    return choice;
-};
+let humanChoice = '';
+const choices = document.querySelectorAll(".choice");
+choices.forEach((choice) => {
+    choice.addEventListener("click", () => {
+        humanChoice = choice.textContent;
+        playRound(humanChoice, getComputerChoice());
+        console.log(`Human Score: ${humanScore} -- Computer Score: ${computerScore}`)
+    });
+});
 
-let playGame = () => {
+function playRound(humanChoice, computerChoice) {
+    humanChoice = humanChoice.toLowerCase();
+    if (humanChoice === computerChoice) {
+        console.log("It's a tie.")
+    } else if (humanChoice === 'rock') {
+        if (computerChoice === 'paper') {
+            console.log("You lose! Paper beats Rock.")
+            computerScore++
+        } else {
+            console.log("You win! Rock beats Scissors.")
+            humanScore++
+        }
+    } else if (humanChoice === 'paper') {
+        if (computerChoice === 'scissors') {
+            console.log("You lose! Scissors beats Paper.")
+            computerScore++
+        } else {
+            console.log("You win! Paper beats Rock.")
+            humanScore++
+        }
+    } else if (humanChoice === 'scissors') {
+        if (computerChoice === 'rock') {
+            console.log("You lose! Rock beats Scissors.")
+            computerScore++
+        } else {
+            console.log("You win! Scissors beats Paper.")
+            humanScore++
+        }
+    }
+}
+
+
+/*let playGame = () => {
     let humanScore = 0;
     let computerScore = 0;
 
@@ -73,5 +113,4 @@ let playGame = () => {
 
     return `${winner} wins the Game`
 
-};
-console.log(playGame())
+};*/
